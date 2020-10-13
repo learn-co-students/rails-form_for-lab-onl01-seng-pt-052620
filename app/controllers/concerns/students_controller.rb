31b0ca7e3448d79ce3b0f8e1cf9dc84494a9c57f
Jack W. Goode <jack.w.goode@icloud.com>
2020-10-13 08:24:54 -0400

    Done.

--- app/controllers/concerns/students_controller.rb
+++ app/controllers/concerns/students_controller.rb
@@ -1,2 +1,44 @@
 class StudentsController < ApplicationController
+
+    def show
+        set_student
+    end
+
+    def new
+        @student = Student.new        
+    end
+
+    def create
+        @student = Student.new(student_params)
+        if @student.save
+            redirect_to student_path(@student)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        set_student        
+    end
+
+    def update
+        set_student
+        if @student.update(student_params)
+            redirect_to student_path(@student)
+        else
+            render :edit
+        end
+
+    end
+
+ 
+    private
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name)        
+    end
+
+    def set_student
+        @student = Student.find(params[:id])
+    end
 end
--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,15 +1,48 @@
 class SchoolClassesController < ApplicationController
 
+    def new
+        @school_class = SchoolClass.new
+        
+    end
+
+    def create
+        @school_class = SchoolClass.new(school_class_params)
+        if @school_class.save
+            redirect_to school_class_path(@school_class)
+        else
+            render :new
+        end
+    end
+
     
     
     
     def show
-        @school_class = SchoolClass.find(school_class_params)
+        @school_class = SchoolClass.find(params[:id])
     end
 
+    def edit
+        set_school_class        
+    end
+
+    def update
+        set_school_class
+        if @school_class.update(school_class_params)
+            redirect_to school_class_path(@school_class)
+        else
+            render :edit
+        end
+
+    end
+
+ 
     private
 
     def school_class_params
-        
+        params.require(:school_class).permit(:title, :room_number)        
+    end
+
+    def set_school_class
+        @school_class = SchoolClass.find(params[:id])
     end
 end
